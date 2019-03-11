4ef2a616df87667505eab79d18ce65d69da2a2a7
Les Aker <me@lesaker.org>
2019-03-11 14:04:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+quote-auth
+===========
+
+[![Build Status](https://img.shields.io/travis/com/configs-akerl/quote-auth.svg)](https://travis-ci.com/configs-akerl/quote-auth)
---
+++ config.yaml.js
@@ -0,0 +1,5 @@
+clientsecret: {{ env['CLIENTSECRET'] }}
+clientid: {{ env['CLIENTID'] }}
+signkey: {{ env['SIGNKEY'] }}
+enckey: {{ env['ENCKEY'] }}
+domain: coolquotes.xyx
